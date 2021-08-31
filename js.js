46ab348c019c5a5b1808c50b2fe02dea3e873bb3
joas24 <88119386+joas24@users.noreply.github.com>
2021-08-31 13:35:38 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+var block = {
+    mon : function(){
+        $('#mon').css('display', 'table');
+    },
+    tue : function(){
+        $('#tue').css('display', 'table');
+    },
+    wed : function(){
+        $('#wed').css('display', 'table');
+    },
+    thu : function(){
+        $('#thu').css('display', 'table');
+    },
+    fri : function(){
+        $('#fri').css('display', 'table');
+    }
+}
+function none(){
+    $('#mon').css('display', 'none');
+    $('#tue').css('display', 'none');
+    $('#wed').css('display', 'none');
+    $('#thu').css('display', 'none');
+    $('#fri').css('display', 'none');
+}
